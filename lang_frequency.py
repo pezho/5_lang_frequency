be1a77657460d6a83c23fbcc42c7f6c4b8411c2d
pezho <pezho.i@mail.ru>
2017-01-11 20:03:44 +0200

    write get_most_frequent_words functions

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
 
+import re
+from collections import Counter
+
 
 def load_data(filepath):
+    """В том случае если файл большой лучше читать его построчно"""
     pass
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(filepath, minlen=0):
+    """
+    
+    """
+    worlds_count = Counter()
+    for line in open(filepath, encoding='utf8'):
+        line = re.sub(r'[,.!@#$%^&*()=+_?\/—]', '', line)
+        for world in line.lower().split():
+            if len(world) < minlen:
+                continue
+            worlds_count[world] += 1
+            
+    return worlds_count.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = 'РУСЛАН И ЛЮДМИЛА.txt'
+    print('\n'.join(map(lambda w: '%s: %s' % w, get_most_frequent_words(filepath))))
